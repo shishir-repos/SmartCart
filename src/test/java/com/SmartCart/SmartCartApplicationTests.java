4c99361abefbdc827e56bfcc8030771d627e5969
Shishir Yadav <60498304+shishir-repos@users.noreply.github.com>
2025-12-21 18:53:12 +0530

    Initial commit

---
+++ src/test/java/com/SmartCart/SmartCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SmartCart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
